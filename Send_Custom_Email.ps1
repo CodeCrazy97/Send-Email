4851a6296c9318b14eb372319ad08a4f69832a09
CodeCrazy97 <evaughan72@yahoo.com>
2024-08-23 14:35:07 -0400

    Added try catch

--- Send_Custom_Email.ps1
+++ Send_Custom_Email.ps1
@@ -8,18 +8,38 @@ $Recipient = Read-Host 'Enter the recipient email address: ';
 
 function Send-ToEmail([string]$email, [string]$attachmentpath){
 
-    $message = new-object Net.Mail.MailMessage;
-    $message.From = $Username;
-    $message.To.Add($email);
-    $message.Subject = Read-Host 'Enter the subject: '
-    $message.Body = Read-Host 'Enter the body: '
-    $attachment = New-Object Net.Mail.Attachment($attachmentpath);
-    $message.Attachments.Add($attachment);
-    $smtp = new-object Net.Mail.SmtpClient($HostCust, $PortCust);
-    $smtp.EnableSSL = $true;
-    $smtp.Credentials = New-Object System.Net.NetworkCredential($Username, $Password);
-    $smtp.send($message);
-    write-host "Mail Sent" ; 
-    $attachment.Dispose();
+    try {
+        $message = New-Object Net.Mail.MailMessage
+        $message.From = $Username
+        $message.To.Add($email)
+        $message.Subject = Read-Host 'Enter the subject: '
+        $message.Body = Read-Host 'Enter the body: '
+        
+        if (Test-Path $attachmentpath) {
+            $attachment = New-Object Net.Mail.Attachment($attachmentpath)
+            $message.Attachments.Add($attachment)
+        } else {
+            Write-Host "Attachment path is invalid."
+        }
+        
+        $smtp = New-Object Net.Mail.SmtpClient($HostCust, $PortCust)
+        $smtp.EnableSSL = $true
+        $smtp.Credentials = New-Object System.Net.NetworkCredential($Username, $Password)
+        
+        # Attempt to send the email
+        $smtp.Send($message)
+        
+        Write-Host "Email sent successfully."
+    }
+    catch {
+        Write-Host "Failed to send email. Error message: $_"
+    }
+    finally {
+        # Dispose of resources
+        $message.Dispose()
+        if ($message.Attachments.Count -gt 0) {
+            $message.Attachments.Dispose()
+        }
+    }    
  }
 Send-ToEmail  -email $Recipient -attachmentpath $Path;
